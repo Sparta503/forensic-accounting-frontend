b1755886bb15e2f9d8cbb5de136e7b53b61365b5
DiorHomme <chomunodisaa68@gmail.com>
2026-04-17 13:02:33 +0200

    Create page.js

---
+++ app/dashboard/admin/users/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+import StaffOverview from "../../../components/users/user";
+
+export default function FraudLogsPage() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-white-900 via-amber-800 to-black p-6 -m-6">
+      <StaffOverview/>
+    </div>
+  );
+}
