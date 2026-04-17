8e978d2573ecb6b61da99198f71dc8d5136d7b34
DiorHomme <chomunodisaa68@gmail.com>
2026-04-17 18:20:12 +0200

    Update page.js

--- app/dashboard/management/page.js
+++ app/dashboard/management/page.js
@@ -15,7 +15,7 @@ import {
 // ✅ CHARTS
 import FraudTrendChart from "../../components/charts/FraudPieChart";
 import RiskPieChart from "../../components/charts/RiskChart";
-import TransactionTrendChart from "../../components/charts/TrendChart";
+import TransactionTrendChart from "../../components/charts/Trendchart";
 
 // Force dynamic rendering to prevent static generation errors
 export const dynamic = "force-dynamic";
