eab667c06e68ac1dcd015867e7d729521fad459d
DiorHomme <chomunodisaa68@gmail.com>
2026-04-17 18:09:34 +0200

    Update page.js

--- app/dashboard/admin/Reports/page.js
+++ app/dashboard/admin/Reports/page.js
@@ -2,6 +2,8 @@
 
 import StaffOverview from "../../../components/Admin reports/reports";
 
+export const dynamic = "force-dynamic";
+
 export default function FraudLogsPage() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-white-900 via-amber-800 to-black p-6 -m-6">
