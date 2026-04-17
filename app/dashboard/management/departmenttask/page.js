1875d632fb2d6f3e1320149b9009f6e3a5113a50
DiorHomme <chomunodisaa68@gmail.com>
2026-04-17 13:03:45 +0200

    Create page.js

---
+++ app/dashboard/management/departmenttask/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+import FraudLogs from "../../../components/Department tasks/departmenttask";
+
+export default function FraudLogsPage() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-white-900 via-amber-800 to-black p-6 -m-6">
+      <FraudLogs />
+    </div>
+  );
+}
