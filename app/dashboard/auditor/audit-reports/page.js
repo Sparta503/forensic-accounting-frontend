74f0da6b2482a371b1bac2bcbcdf8023a8ba7717
DiorHomme <chomunodisaa68@gmail.com>
2026-04-17 18:09:57 +0200

    Update page.js

--- app/dashboard/auditor/audit-reports/page.js
+++ app/dashboard/auditor/audit-reports/page.js
@@ -2,6 +2,8 @@
 
 import FraudLogs from "../../../components/AuditReports/auditreports";
 
+export const dynamic = "force-dynamic";
+
 export default function FraudLogsPage() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-white-900 via-amber-800 to-black p-6 -m-6">
