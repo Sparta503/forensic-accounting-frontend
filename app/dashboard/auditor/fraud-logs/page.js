5327b468fc9fc418ba99f7ee8afb8feae54a8944
DiorHomme <chomunodisaa68@gmail.com>
2026-04-17 13:03:14 +0200

    Update page.js

--- app/dashboard/auditor/fraud-logs/page.js
+++ app/dashboard/auditor/fraud-logs/page.js
@@ -4,7 +4,7 @@ import FraudLogs from "../../../components/fraudlogs/fraudlog";
 
 export default function FraudLogsPage() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-yellow-900 via-amber-800 to-black p-6 -m-6">
+    <div className="min-h-screen bg-gradient-to-br from-white-900 via-amber-800 to-black p-6 -m-6">
       <FraudLogs />
     </div>
   );
