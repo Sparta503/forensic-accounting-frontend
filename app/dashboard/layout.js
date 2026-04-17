e25c574de06092ca7c872d1305e70eae675e5f8d
DiorHomme <chomunodisaa68@gmail.com>
2026-04-17 16:12:32 +0200

    Update layout.js

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -3,13 +3,15 @@ import Navbar from "../components/layout/Navbar";
 
 export default function DashboardLayout({ children }) {
   return (
-    <div className="flex">
-      <Sidebar />
+    <div className="flex h-screen overflow-hidden">
+      <div className="flex-shrink-0">
+        <Sidebar />
+      </div>
 
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col overflow-hidden">
         <Navbar />
 
-        <main className="p-6 bg-gray-100 min-h-screen">
+        <main className="flex-1 p-6 bg-gray-100 overflow-y-auto">
           {children}
         </main>
       </div>
